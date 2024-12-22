2dc1ef7cf8d81e2cf482529af4d4fe96d9e4b0e4
Frank732 <hacphran122@gmail.com>
2024-12-22 23:21:57 +0000

    trip detail route fixed

--- frontend/src/components/pages/Trips/trip-card.tsx
+++ frontend/src/components/pages/Trips/trip-card.tsx
@@ -117,7 +117,7 @@ const TripCard: React.FC<TripCardProps> = ({
   return isAdmin ? (
     cardContent
   ) : (
-    <Link to={`/trips/${trip?._id}`} className="block">
+    <Link to={`/ASGSDWSDZ-234ADFSDAS/trip/${trip?._id}`} className="block">
       {cardContent}
     </Link>
   );
