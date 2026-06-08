eecb1156ad1dfad5198ed6768909d739df0c0e51
Frank Addai <faddai@acsint.io>
2026-06-08 21:58:37 +0000

    empty product changes

--- frontend/src/components/pages/product/products.tsx
+++ frontend/src/components/pages/product/products.tsx
@@ -115,8 +115,8 @@ const StorePage: React.FC = () => {
               </div>
             ) : products.length === 0 && !isSuggestion ? (
               <div className="flex flex-col items-center justify-center py-20 text-center">
-                <div className="bg-gray-100 p-6 rounded-full border border-gray-200 mb-6 shadow-xl">
-                  <ShoppingCart className="w-16 h-16 text-[#8b7355] opacity-50" />
+                <div className="bg-black p-6 rounded-full border border-gray-200 mb-6 shadow-xl">
+                  <ShoppingCart color='#8b7355' className="w-16 h-16 opacity-50" />
                 </div>
                 <h3 className="text-3xl font-serif text-gray-900 mb-3 font-bold">
                   No Products Found
